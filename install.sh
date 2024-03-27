085575595fda8099330c6b3d424228badad0ac00
dikamenchanel <101949759+dikamenchanel@users.noreply.github.com>
2024-03-27 18:20:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@ cp /home/$USER/aliases_shell/.alias_shell /home/$USER/
 &&
 if [ -f "/home/$USER/.zshrc" ] ; then
   echo "source ~/.alias_shell" >> ~/.zshrc
+  &&
+  source ~/.zshrc
 fi
 
 if [ -f "/home/$USER/.bashrc" ] ; then
   echo "source ~/.alias_shell" >> ~/.bashrc
+  &&
+  source ~/.bashrc
 fi
